cfec6befd5ca0dce620c2bafd7cb52c263c3224d
DanialsRT <daniilnavolockii@mail.ru>
2021-10-19 21:06:09 +0300

    1

--- Пример/Java.js
+++ Пример/Java.js
@@ -1,19 +1,88 @@
 function highlighting(){
-    d = new Date();
-    time = d.getDate()+"."+(d.getMonth()+1)+"."+(d.getYear()-100);
-    return time;
+  currentdate = new Date();
+  var oneJan = new Date(currentdate.getFullYear(),0,1);
+  var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
+  var result = Math.ceil(( currentdate.getDay() + 1 + numberOfDays) / 7);
+  if(result / 2 == 1)
+  {
+    result = "Числитель";
   }
+  else
+  {
+    result = "Знаменатель";
+  }
+  document.getElementById('result').innerHTML = result;
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var time = 0;
+  hours = hours * 100;
+  time = hours + minutes;
+  document.getElementById('timedisplay1').innerHTML = time;
+  time = 1000;
+  if(time > 900 && time <= 1105 && result == "Числитель")
+  {
+    document.getElementById('pn1').style.backgroundColor = 'red';
+    document.getElementById('pn1').style.color = 'white';
+  }
+  if(time > 900 && time <= 1105 && result == "Знаменатель")
+  {
+    document.getElementById('pn2').style.backgroundColor = 'red';
+    document.getElementById('pn2').style.color = 'white';
+  }
+  if(time > 1105 && time <= 1250 && result == "Числитель")
+  {
+    document.getElementById('pn3').style.backgroundColor = 'red';
+    document.getElementById('pn3').style.color = 'white';
+  }
+  if(time > 1105 && time <= 1250 && result == "Знаменатель")
+  {
+    document.getElementById('pn4').style.backgroundColor = 'red';
+    document.getElementById('pn4').style.color = 'white';
+  }
+  if(time > 1300 && time <= 1510 && result == "Числитель")
+  {
+    document.getElementById('pn5').style.backgroundColor = 'red';
+    document.getElementById('pn5').style.color = 'white';
+  }
+  if(time > 1300 && time <= 1510 && result == "Знаменатель")
+  {
+    document.getElementById('pn6').style.backgroundColor = 'red';
+    document.getElementById('pn6').style.color = 'white';
+  }
+  if(time > 1510 && time <= 1655 && result == "Числитель")
+  {
+    document.getElementById('pn7').style.backgroundColor = 'red';
+    document.getElementById('pn7').style.color = 'white';
+  }
+  if(time > 1510 && time <= 1655 && result == "Знаменатель")
+  {
+    document.getElementById('pn8').style.backgroundColor = 'red';
+    document.getElementById('pn8').style.color = 'white';
+  }
+}
+setInterval(highlighting, 0);
 
 function getDate(){
     var date = new Date();
+    var day = date.getDate();
+    var month = (date.getMonth() + 1);
+    var year = (date.getYear()-100);
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var seconds = date.getSeconds();
-    //По надобности условие ниже повторить с minutes и hours
     if(seconds < 10)
     {
         seconds = '0' + seconds;
     }
-    document.getElementById('timedisplay').innerHTML = hours + ':' + minutes + ':' + seconds;
+    if(minutes < 10)
+    {
+        minutes = '0' + minutes;
+    }
+    if(hours < 10)
+    {
+        hours = '0' + hours;
+    }
+    document.getElementById('timedisplay').innerHTML = day + '.' + month + '.' + year + ' ' + hours + ':' + minutes + ':' + seconds;
 }
 setInterval(getDate, 0);
---
+++ Расписание/Java.js
@@ -0,0 +1,174 @@
+function highlighting(){
+    currentdate = new Date();
+    var oneJan = new Date(currentdate.getFullYear(),0,1);
+    var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
+    var result = Math.ceil(( currentdate.getDay() + 1 + numberOfDays) / 7);
+    if(result / 2 == 1)
+    {
+      result = "Числитель";
+    }
+    else
+    {
+      result = "Знаменатель";
+    }
+    document.getElementById('result').innerHTML = result;
+    var days = ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'];
+    var ds = new Date();
+    var n = ds.getDay();
+    //document.getElementById('n').innerHTML = n;
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var time = 0;
+    hours = hours * 100;
+    time = hours + minutes;
+    //document.getElementById('timedisplay1').innerHTML = time;
+    if(time > 900 && time <= 1105)
+    {
+        if(n == 1)
+        {
+            if(result == "Числитель")
+            {
+                document.getElementById('pn11').style.color = 'red';
+            }
+            else
+            {
+                document.getElementById('pn12').style.color = 'red';
+            }
+        }
+        if(n == 2)
+        {
+            document.getElementById('vt1').style.color = 'red';
+        }
+        if(n == 3)
+        {
+            if(result == "Числитель")
+            {
+                document.getElementById('sr11').style.color = 'red';
+            }
+            else
+            {
+                document.getElementById('sr12').style.color = 'red';
+            }
+        }
+        if(n == 4)
+        {
+            if(result == "Числитель")
+            {
+                document.getElementById('ch11').style.color = 'red';
+            }
+            else
+            {
+                document.getElementById('ch12').style.color = 'red';
+            }
+        }
+        if(n == 5)
+        {
+            document.getElementById('pt1').style.color = 'red';
+        }
+    }
+    if(time > 1105 && time <= 1250)
+    {
+        if(n == 1)
+        {
+            document.getElementById('pn2').style.color = 'red';
+        }
+        if(n == 2)
+        {
+            document.getElementById('vt2').style.color = 'red';
+        }
+        if(n == 3)
+        {
+            document.getElementById('sr2').style.color = 'red';
+        }
+        if(n == 4)
+        {
+            if(result == "Числитель")
+            {
+                document.getElementById('ch211').style.color = 'red';
+                document.getElementById('ch212').style.color = 'red';
+            }
+            else
+            {
+                document.getElementById('ch221').style.color = 'red';
+                document.getElementById('ch222').style.color = 'red';
+            }
+        }
+        if(n == 5)
+        {
+            document.getElementById('pt2').style.color = 'red';
+        }
+    }
+    if(time > 1300 && time <= 1510)
+    {
+        if(n == 1)
+        {
+            document.getElementById('pn3').style.color = 'red';
+        }
+        if(n == 2)
+        {
+            document.getElementById('vt3').style.color = 'red';
+        }
+        if(n == 3)
+        {
+            if(result == "Числитель")
+            {
+                document.getElementById('sr3').style.color = 'red';
+            }
+        }
+        if(n == 4)
+        {
+            document.getElementById('ch3').style.color = 'red';
+        }
+        if(n == 5)
+        {
+            document.getElementById('pt3').style.color = 'red';
+        }
+    }
+    if(time > 1510 && time <= 1655)
+    {
+        if(n == 2)
+        {
+            document.getElementById('vt4').style.color = 'red';
+        }
+        if(n == 3)
+        {
+            if(result == "Числитель")
+            {
+                document.getElementById('sr4').style.color = 'red';
+            }
+        }
+        if(n == 4)
+        {
+            if(result == "Числитель")
+            {
+                document.getElementById('ch4').style.color = 'red';
+            }
+        }
+    }
+  }
+  setInterval(highlighting, 0);
+  
+  function getDate(){
+      var date = new Date();
+      var day = date.getDate();
+      var month = (date.getMonth() + 1);
+      var year = (date.getYear()-100);
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var seconds = date.getSeconds();
+      if(seconds < 10)
+      {
+          seconds = '0' + seconds;
+      }
+      if(minutes < 10)
+      {
+          minutes = '0' + minutes;
+      }
+      if(hours < 10)
+      {
+          hours = '0' + hours;
+      }
+      //document.getElementById('timedisplay').innerHTML = day + '.' + month + '.' + year + ' ' + hours + ':' + minutes + ':' + seconds;
+  }
+  setInterval(getDate, 0);
