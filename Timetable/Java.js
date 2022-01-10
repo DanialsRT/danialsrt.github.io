92e0f95181c6d5b173b2c9bee388cc9f04ef09b5
DanialsRT <daniilnavolockii@mail.ru>
2022-01-10 15:46:55 +0300

    1

--- Timetable/Java.js
+++ Timetable/Java.js
@@ -168,7 +168,34 @@ function getDate() {
 //setInterval(getDate, 0);
 
 var Reloaded = function () {
-  location.replace("../Index.html");
+  Date.prototype.getWeek = function () {
+    var date = new Date(this.getTime());
+    date.setHours(0, 0, 0, 0);
+    date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+    var week1 = new Date(date.getFullYear(), 0, 4);
+    result =
+      1 +
+      Math.round(
+        ((date.getTime() - week1.getTime()) / 86400000 -
+          3 +
+          ((week1.getDay() + 6) % 7)) /
+          7
+      );
+    return result;
+  };
+  var mydate = new Date();
+  result = mydate.getWeek();
+  if (result % 2 == 1) {
+    if (location.href == "https://danialsrt.github.io/Timetable/Index2.html") {
+    } else {
+      location.replace("https://danialsrt.github.io/Timetable/Index2.html");
+    }
+  } else {
+    if (location.href == "https://danialsrt.github.io/Timetable/Index1.html") {
+    } else {
+      location.replace("https://danialsrt.github.io/Timetable/Index1.html");
+    }
+  }
 }; //страницу перезагрузили
 
 window.onload = function () {
